ad6f5b6ecb7d09486f2b5ff408c6cd8dbd83d2f7
yangjoker <jeallen888@icloud.com>
2020-12-23 15:37:56 +0800

    change method

--- DIDI/ViewController.swift
+++ DIDI/ViewController.swift
@@ -13,6 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        
+    }
 
 
 }
